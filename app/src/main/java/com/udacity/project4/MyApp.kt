b3e266871e522efdee2acfeb7a20fd71c27e46e8
Romi Agar <3225967+r0mi@users.noreply.github.com>
2021-06-23 23:59:52 +0300

    Add Timber logging dependency

--- app/src/main/java/com/udacity/project4/MyApp.kt
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -10,11 +10,21 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
+import timber.log.Timber
+
+import timber.log.Timber.DebugTree
+
+
+
 
 class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
 
         /**
          * use Koin Library as a service locator
