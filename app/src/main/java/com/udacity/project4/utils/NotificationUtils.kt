e13a6b807585c66444cc310ca1c7ddd4ee2014cf
Romi Agar <3225967+r0mi@users.noreply.github.com>
2021-09-29 11:05:32 +0300

    Use extension function for notificationManager & utility function for checking for Oreo

--- app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
+++ app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
@@ -4,7 +4,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import androidx.core.content.ContextCompat
@@ -16,20 +15,17 @@ import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 private const val NOTIFICATION_CHANNEL_ID = BuildConfig.APPLICATION_ID + ".channel"
 
 fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
-    val notificationManager = context
-        .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
     // We need to create a NotificationChannel associated with our CHANNEL_ID before sending a notification.
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
-        && notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null
-    ) {
-        val name = context.getString(R.string.app_name)
-        val channel = NotificationChannel(
-            NOTIFICATION_CHANNEL_ID,
-            name,
-            NotificationManager.IMPORTANCE_DEFAULT
-        )
-        notificationManager.createNotificationChannel(channel)
+    ifRunningOreoOrLater {
+        if (context.notificationManager?.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null) {
+            val name = context.getString(R.string.app_name)
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                name,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            context.notificationManager?.createNotificationChannel(channel)
+        }
     }
 
     val intent = ReminderDescriptionActivity.newIntent(context.applicationContext, reminderDataItem)
@@ -51,7 +47,7 @@ fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
         .setAutoCancel(true)
         .build()
 
-    notificationManager.notify(getUniqueId(), notification)
+    context.notificationManager?.notify(getUniqueId(), notification)
 }
 
 private fun getUniqueId() = ((System.currentTimeMillis() % 10000).toInt())
